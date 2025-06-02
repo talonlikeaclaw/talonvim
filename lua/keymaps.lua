8cb2f3153f969ff888bbea8fad1edda0b3b0bbe3
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-06-01 21:07:56 -0400

    refactor: modularize options, keymaps, and lazy bootstrap into seperate files

---
+++ lua/keymaps.lua
@@ -0,0 +1,47 @@
+----- [[ Basic Keymaps ]] -----
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Enable Oil file view functionality
+vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'open parent directory' })
+
+-- Save buffer
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<leader>cs', '<cmd>w<cr><esc>', { desc = 'save file' })
+
+-- Quit buffer
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'quit buffer', noremap = true, silent = true })
+
+-- Quit Talonvim
+vim.keymap.set('n', '<leader>Q', '<cmd>qa!<cr>', { desc = 'quit talonvim', noremap = true, silent = true })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>w-', '<cmd>sp<CR>', { desc = 'split across', remap = true })
+vim.keymap.set('n', '<leader>w|', '<cmd>vs<CR>', { desc = 'split down', remap = true })
+
+-- Highlight when yanking (copying) text
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/options.lua
@@ -0,0 +1,74 @@
+----- [[ Setting options ]] -----
+-- See `:help vim.o`
+--  For more options, you can see `:help option-list`
+
+-- Make Markdown hide syntax characters
+vim.o.conceallevel = 1
+
+-- Disable line wrapping
+vim.o.wrap = false
+
+-- Prevent wrapping in middle of words
+vim.o.linebreak = true
+
+-- Make line numbers default
+vim.o.number = true
+
+-- Enable relative line numbers
+vim.o.relativenumber = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- Confirm if quiting without saving
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+-- vim: ts=2 sts=2 sw=2 et
