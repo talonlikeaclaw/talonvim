6bbb98d85f3552fe7cf04691c4e9c664ffa82b68
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-06-04 20:02:03 -0400

    feat: add toggle-term for quick and easy access to a terminal in talonvim

---
+++ lua/custom/plugins/toggle-term.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      size = 20,
+      open_mapping = [[<leader>t]],
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      persist_size = true,
+      direction = 'horizontal',
+      close_on_exit = true,
+      shell = vim.o.shell,
+    },
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+    end,
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
